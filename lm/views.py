53fa89f7e24ceccf4df6f963d96d728f1789d180
elena_pantsurkina <pantsurkina.e@bk.ru>
2025-02-25 21:57:15 +0700

    поправила метод get_permission

--- lm/views.py
+++ lm/views.py
@@ -18,9 +18,7 @@ class LearningVieSet(ModelViewSet):
         lm.save()
 
     def get_permissions(self):
-        """Метод определения действий в зависимости является ли пользователь  владельцем."""
-        if self.action in ["update", "retrieve"]:
-            self.permission_classes = (IsOwner,)
-        elif self.action == "destroy":
+        """Метод определения действий в зависимости от того, является ли пользователь владельцем."""
+        if self.action in ["update", "retrieve", "destroy"]:
             self.permission_classes = (IsOwner,)
         return super().get_permissions()
